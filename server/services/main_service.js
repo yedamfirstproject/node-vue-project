536d27e3d09bd5c864c75f5dcee30dab59fde63a
taehotaeho <jackt0506@naver.com>
2026-03-24 10:18:04 +0900

    백 코드 수정/메인페이지/20260324

--- server/services/main_service.js
+++ server/services/main_service.js
@@ -5,25 +5,35 @@ const mainMapper = require("../database/mappers/main_mapper.js");
 // 메인페이지 조사지 내역 조회
 const findById = async (id) => {
   let list = await mainMapper.selectById(id);
-  // 우선순위코드 => 등급
-  let rank = list.result;
-  switch (rank) {
-    case "f003":
-      rank = "긴급";
-      break;
-    case "f002":
-      rank = "중점";
-      break;
-    case "f001":
-      rank = "계획";
-      break;
 
-    default:
-      rank = "심사중";
-      break;
-  }
-  list.result = rank;
-  return list;
+  if (!list || list.length == 0) return [];
+  // 우선순위코드 => 등급
+  let formattedList = list.map((item) => {
+    let rankName = "";
+
+    switch (item.priorityCode) {
+      case "f003":
+        rankName = "긴급";
+        break;
+      case "f002":
+        rankName = "중점";
+        break;
+      case "f001":
+        rankName = "계획";
+        break;
+      default:
+        rankName = "심사중";
+        break;
+    }
+
+    item.priorityCode = rankName;
+    item.hasPlan = item.planId ? true : false;
+    item.hasResult = item.resultId ? true : false;
+
+    return item;
+  });
+
+  return formattedList;
 };
 
 module.exports = {
